7ac34856b31e5685fc9c91373bbb9b2338976ab0
Canta Tronic <tokareva@jinr.ru>
2017-11-29 18:34:32 +0300

    Get device from command line in a very accurate mode

---
+++ include/deviceType.h
@@ -0,0 +1,4 @@
+
+#pragma once 
+
+enum deviceType {CPU, GPU, PHI};
--- libCommon/TUtils.h
+++ libCommon/TUtils.h
@@ -3,6 +3,13 @@
 
 #include <iostream>
 #include <stdlib.h>
+#include <omp.h>
+#include <string>
+// #include <cstring>
+#include <stdexcept>
+#include <exception>
+#include "deviceType.h"
+#include <cstdlib> 
 
 using std::cout;
 using std::endl;
@@ -30,10 +37,49 @@ void argCtrl(int argc, char * argv[]){
 }
 
 void setThrNums(int argc, char * argv[]){
-//     argCtrl(argc, argv);  //check out the arguments given
     unsigned thrNum = 1;	//init thrNum
     if(argc > 1 && argv[1]>0) 
             thrNum = atoi(argv[1]);
     omp_set_num_threads(thrNum);  //set number of threads
     testThrNumbs(); //checkout whether theads were set up
+}
+
+void devPrint(std::string devName)  {
+  cout<<"Starting "<<devName<<" calculations..."<<endl;
+}
+
+deviceType setDevice(int argc, char * argv[]){
+    if (argc > 2){
+        std::string dev = std::string(argv[2]);
+        if (dev == "CPU") {
+          devPrint(dev);
+          return CPU;
+        } else if (dev == "GPU") {
+          devPrint(dev);
+          return GPU;
+        } else if (dev == "PHI") {
+          devPrint(dev);
+          return CPU;
+        } else {
+          throw std::domain_error ("Undefined device! Exiting...");
+        }
+    } else {
+        throw std::domain_error ("No device provided! Exiting...");
+    }
+}
+
+int returnNum (int num) {
+  //simplest function to test tests
+  return num;
+}
+
+deviceType setArguments(int argc, char * argv[]) {
+  setThrNums(argc, argv); 
+  try {
+    deviceType dev = setDevice(argc, argv);
+    return dev;
+  } catch (std::exception const& exc) {
+    std::cerr<<exc.what()<<endl;
+    exit(0);
+  }
 }
